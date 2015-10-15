8e973492134912d282f5e59cbe9f8cdfcbfe1613
Khanh Acoustic <nxkhanh.qt@gmail.com>
2015-10-15 10:07:36 +0700

    Add models Change JRE version to 1.7

---
+++ FoodSocial/src/model/bean/Place.java
@@ -0,0 +1,34 @@
+package model.bean;
+
+public class Place {
+	
+	private String placeId;
+	private String placeName;
+	private double longitude;
+	private double latitude;
+	
+	public String getPlaceId() {
+		return placeId;
+	}
+	public void setPlaceId(String placeId) {
+		this.placeId = placeId;
+	}
+	public String getPlaceName() {
+		return placeName;
+	}
+	public void setPlaceName(String placeName) {
+		this.placeName = placeName;
+	}
+	public double getLongitude() {
+		return longitude;
+	}
+	public void setLongitude(double longitude) {
+		this.longitude = longitude;
+	}
+	public double getLatitude() {
+		return latitude;
+	}
+	public void setLatitude(double latitude) {
+		this.latitude = latitude;
+	}
+}
---
+++ FoodSocial/src/model/bean/Post.java
@@ -0,0 +1,13 @@
+package model.bean;
+
+public class Post {
+
+	private String postId;
+	private String userId;
+	private String content;
+	private String imageUrl;
+	private String placeId;
+	
+	private int numOfVotes;
+	private int numOfComments;
+}
